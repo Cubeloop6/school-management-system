b0fdf604a43a6b3792894b19e332b53bf4686ef9
Cubeloop6 <rockiseverything12345@gmail.com>
2020-11-22 01:22:06 -0800

    added teachers class and added more constructors in student

--- School Management System/src/Student.java
+++ School Management System/src/Student.java
@@ -7,10 +7,20 @@ public class Student {
 	private int feesPaid;
 	private int feesTotal;
 	public Student(int id, String name, int grade) {
+		this.feesPaid = 0;
+		this.feesTotal = 30000;
 		this.id = id;
 		this.name = name;
 		this.grade = grade;
 	}
 	
+	
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
+	public void updateFeesPaid(int fees) {
+		this.feesPaid += fees;
+	}
 	
 }
