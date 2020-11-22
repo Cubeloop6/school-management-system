492512e67425d8730b3268aeee3d177d3dfe9522
Cubeloop6 <rockiseverything12345@gmail.com>
2020-11-22 02:00:29 -0800

    added constructor

--- School Management System/src/School.java
+++ School Management System/src/School.java
@@ -1,6 +1,16 @@
+import java.util.List;
 
 public class School {
 	
-	private Teacher teacher;
-	private Student student;
+	private List<Teacher> teacher;
+	private List<Student> student;
+	private int totalMoneyEarned;
+	private int totalMoneySpent;
+	public School(List<Teacher> teacher, List<Student> student) {
+		this.teacher = teacher;
+		this.student = student;
+	}
+	
+	
+	
 }
